4c948d48aa83a658e232645043d209d343021fd4
KateZhukel <36920026+KateZhukel@users.noreply.github.com>
2018-09-30 22:23:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let res = 0;
+    base = getMaxSimpleDivision(base);
+    while(number > 0) {
+        number = div(number, base);
+        res += number;
+    }
+    return res;
+};
+
+function div(x, y){
+    return (x - x % y) / y;
+}
+
+function getMaxSimpleDivision(number){
+    let max;
+    let division = 2;
+    while(number !== 1){
+        if(number % division !== 0){
+            division++;
+        } else{
+            number = div(number,division);
+            max = division;
+        }
+    }
+    return max;
 }
